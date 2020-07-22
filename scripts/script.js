7126d9ccd02e07a74ca004c07de4e1e891d3f9c6
Nivedin <nivedinp@gmail.com>
2020-07-23 00:29:44 +0530

    Modified JS

--- scripts/script.js
+++ scripts/script.js
@@ -3,63 +3,63 @@
  * @param err - error thrown by any function
  * @description Helper function to handle errors
  */
-let handleFail = function (err) {
-  console.log("Error : ", err);
-};
+// let handleFail = function (err) {
+//   console.log("Error : ", err);
+// };
 
 // Queries the container in which the remote feeds belong
-let remoteContainer = document.getElementById("remote-container");
-let canvasContainer = document.getElementById("canvas-container");
+// let remoteContainer = document.getElementById("remote-container");
+// let canvasContainer = document.getElementById("canvas-container");
 /**
  * @name addVideoStream
  * @param streamId
  * @description Helper function to add the video stream to "remote-container"
  */
-function addVideoStream(streamId) {
-  let streamDiv = document.createElement("div"); // Create a new div for every stream
-  streamDiv.id = streamId; // Assigning id to div
-  streamDiv.style.transform = "rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
-  remoteContainer.appendChild(streamDiv); // Add new div to container
-}
+// function addVideoStream(streamId) {
+//   let streamDiv = document.createElement("div"); // Create a new div for every stream
+//   streamDiv.id = streamId; // Assigning id to div
+//   streamDiv.style.transform = "rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+//   remoteContainer.appendChild(streamDiv); // Add new div to container
+// }
 /**
  * @name removeVideoStream
  * @param evt - Remove event
  * @description Helper function to remove the video stream from "remote-container"
  */
-function removeVideoStream(evt) {
-  let stream = evt.stream;
-  stream.stop();
-  let remDiv = document.getElementById(stream.getId());
-  remDiv.parentNode.removeChild(remDiv);
-  console.log("Remote stream is removed " + stream.getId());
-}
+// function removeVideoStream(evt) {
+//   let stream = evt.stream;
+//   stream.stop();
+//   let remDiv = document.getElementById(stream.getId());
+//   remDiv.parentNode.removeChild(remDiv);
+//   console.log("Remote stream is removed " + stream.getId());
+// }
 
-function addCanvas(streamId) {
-  let canvas = document.createElement("canvas");
-  canvas.id = "canvas" + streamId;
-  canvasContainer.appendChild(canvas);
-  let ctx = canvas.getContext("2d");
-  let video = document.getElementById(`video${streamId}`);
+// function addCanvas(streamId) {
+//   let canvas = document.createElement("canvas");
+//   canvas.id = "canvas" + streamId;
+//   canvasContainer.appendChild(canvas);
+//   let ctx = canvas.getContext("2d");
+//   let video = document.getElementById(`video${streamId}`);
 
-  video.addEventListener("loadedmetadata", function () {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-  });
+//   video.addEventListener("loadedmetadata", function () {
+//     canvas.width = video.videoWidth;
+//     canvas.height = video.videoHeight;
+//   });
 
-  video.addEventListener(
-    "play",
-    function () {
-      var $this = this; //cache
-      (function loop() {
-        if (!$this.paused && !$this.ended) {
-          ctx.drawImage($this, 0, 0);
-          setTimeout(loop, 1000 / 30); // drawing at 30fps
-        }
-      })();
-    },
-    0
-  );
-}
+//   video.addEventListener(
+//     "play",
+//     function () {
+//       var $this = this; //cache
+//       (function loop() {
+//         if (!$this.paused && !$this.ended) {
+//           ctx.drawImage($this, 0, 0);
+//           setTimeout(loop, 1000 / 30); // drawing at 30fps
+//         }
+//       })();
+//     },
+//     0
+//   );
+// }
 
 var rtc = {
   client: null,
@@ -72,7 +72,7 @@ var rtc = {
 
 var option = {
   appID: "de94b2bb7d2b4657949186c20f531eee",
-  channel: "Channel name",
+  channel: "AgoraVideo",
   uid: null,
   token:
     "006de94b2bb7d2b4657949186c20f531eeeIAA9JbCC2uVK1eWntwdC5prinxihLEbRXtTdw4bu+QGozprNu8cAAAAAEAC+3ac7XM8ZXwEAAQBezxlf",
